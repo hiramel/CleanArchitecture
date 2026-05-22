95e8a8f34a2c78469949fd100bdf5814d65b067e
Hiram Elguézabal Jiménez <hiramel@gmail.com>
2026-05-22 09:44:21 -0600

    Add UnitTest

---
+++ TableCleanArchitectureTests/MockUsersRepository.swift
@@ -0,0 +1,33 @@
+//
+//  MockUsersRepository.swift
+//  TableCleanArchitectureTests
+//
+//  Created by Hiram Elguézabal Jiménez on 22/05/26.
+//
+
+import Foundation
+@testable import TableCleanArchitecture
+
+final class MockUsersRepository: UsersRepository {
+
+    // MARK: - Tracking
+
+    var deleteUserCalled = false
+    var deletedUserId: String?
+
+    // MARK: - Mock Data
+
+    var usersToReturn: [User] = []
+
+    // MARK: - Functions
+
+    func getUsers() async throws -> [User] {
+        usersToReturn
+    }
+
+    func deleteUser(id: String) async throws {
+
+        deleteUserCalled = true
+        deletedUserId = id
+    }
+}
